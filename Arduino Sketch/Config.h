8a695fda0731021128f5f5a629a78f09daa9283c
Lammertus de Vries <158805765+Mert16757@users.noreply.github.com>
2024-05-06 14:49:25 +0200

    Added latest version
    Latest test version trying to get around delay reading TSL237 when very dark.

---
+++ Arduino Sketch/Config.h
@@ -0,0 +1,12 @@
+// Serial connection
+#define SERIAL_SPEED 9600
+
+// Sky Quality Meter
+#define SQM_LIMIT 19.0
+#define SQM_SAMPLES 5
+#define SQM_AVG_DELAY 5
+
+// Light sensor
+//#define LDR A0
+//#define LDR_LOW 100
+//#define LDR_MID 600
